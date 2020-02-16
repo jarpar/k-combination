152600ea0681b034c239b5c43711c2c7f7422cad
jarpar <partyka.jaroslaw@gmail.com>
2020-02-17 00:44:41 +0100

    refactored

--- src/Kcombination.java
+++ src/Kcombination.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class Kcombination {
 
     public static int comb(int n, int k) {
-        int comb;
         if (k == 0) {
-            comb = 1;
+            return 1;
         } else if (k > n) {
-            comb = 0;
+            return 0;
         } else {
-            comb = comb((n - 1), k) + comb((n - 1), (k - 1));
+            return comb((n - 1), k) + comb((n - 1), (k - 1));
         }
-        return comb;
     }
 
     public static void main(String[] args) {
